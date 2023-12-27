166ddd62c36378e57a661d7cd5e147bf7045f430
pooja <poojak1447@gmail.com>
2023-12-27 16:07:15 +0530

    for loop and while loop

---
+++ tutorial/for_loops_basic.py
@@ -0,0 +1,17 @@
+# for loops can iterate over a sequence of iterable objects in python
+name="abhishek"
+for i in name: 
+    print(i)
+colors=["red","green","blue","yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+for k in range(5):
+    print(k)
+for k in range(1,5):
+    print(k)
+for i in range(1,12,3):
+    print(i)
+# here 3 is the difference
+    
---
+++ tutorial/while_loop_basics.py
@@ -0,0 +1,19 @@
+i=0
+while(i<3):
+    print(i)
+    i+=1
+i=int(input("enter the number: "))
+while(i<=38):
+    i=int(input("enter the number"))
+    print(i)
+print("done with loop")
+# reverse while loop
+# decrementing loop
+count=5
+while(count>0):
+    print(count)
+    count-=1
+else:
+    print("i am inside else")
+# as soon as while loop condition becomes false,the else statement is executed
+    
