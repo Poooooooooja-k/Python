c6e70170b592a618a966516a61baebc1ca687734
pooja <poojak1447@gmail.com>
2023-12-29 18:39:31 +0530

    functional arguments and return statement

---
+++ tutorial/functionargument_return_statement.py
@@ -0,0 +1,9 @@
+# default arguments:provides a default value while creating a function
+# keyword arguments
+# variable length arguments
+# required arguments
+def average(a=9,b=1):
+    print("the average is ",(a+b)/2)
+# average(4,6)
+average(5)
+
