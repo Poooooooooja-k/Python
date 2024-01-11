236274ccd08d556cc5c640b168b3b8279b4fb9df
pooja <poojak1447@gmail.com>
2024-01-11 09:55:30 +0530

    dictionary

---
+++ tutorial/dictionary.py
@@ -0,0 +1,11 @@
+# dictionary is an ordered collection of data items .they have key-value pair
+dic={
+    344:"harry",
+    56:"pooja",
+    89:"sumi",
+    567:"sneha"
+}
+# print(dic[567] )
+# print(dic.keys())
+# print(dic.values()).
+# print(dic.items())
