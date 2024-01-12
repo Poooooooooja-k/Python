47fc0395d97d2998064e8f0c0c256bfb0319ae10
pooja <poojak1447@gmail.com>
2024-01-12 14:56:38 +0530

    dictionary functions and exception handling

--- tutorial/dictionary.py
+++ tutorial/dictionary.py
@@ -9,3 +9,16 @@ dic={
 # print(dic.keys())
 # print(dic.values()).
 # print(dic.items())
+
+# update 
+ep1={122:45,123:89,567:69,670:69}
+ep2={222:67,565:90}
+ep1.update(ep2)
+print(ep1)
+# clear()method is used to remove all the items from the dictionary
+ep1.pop(122)
+print(ep1)
+# popitem():method removes the last key-value pair from the dictionary
+ep2.popitem()
+print(ep2)
+# del :delete the whole dictionary
---
+++ tutorial/exception_handling.py
@@ -0,0 +1,18 @@
+
+# exception handling is the process of responding to unwanted or unexpected events when a computer program runs
+a=input("enter the number: ")
+print(f"multiplication table of {a} is: ")
+try:
+    for i in range(1,11):
+        print(f"{int(a)}x{i}={int(a)*i}")
+except:
+    print("invalid input")
+
+
+
+try:
+    num=int(input("enter an integer: "))
+except ValueError:
+    print("number entered is not an integer")
+except IndexError:
+    print("index error")
