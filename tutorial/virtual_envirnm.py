bbce7bd792ce7346a52ab831e4e2b33dc79c9b48
pooja <poojak1447@gmail.com>
2024-01-21 22:22:58 +0530

    virtual enviornment

---
+++ tutorial/virtual_envirnm.py
@@ -0,0 +1,11 @@
+# virtual environment:is used to isolate specific python environments  on a single machine,allowing you to work on multiple projects with diffreent dependencies and packages without conflict
+
+# create a virtual envirnoment
+# python -m venv myenv
+
+
+# activate the virtual evn
+# source myenv/bin/activate 
+
+
+
